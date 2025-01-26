0de95b3164d0cf8ea8453f98f7c4629b2ca47e9b
AmilaWijayananda <amisampath12@gmail.com>
2025-01-26 16:31:49 +0530

    Install tailwind css ✅

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,11 +1,11 @@
-import './loginPage.css'
+import { Link } from 'react-router-dom';
+//import './loginPage.css'
 export default function LoginPage() {
     return(
         <div>
-            <h1>Login Page</h1>
-            <input type="text" placeholder="Enter Your Name"/>
-            <input type="password" placeholder="Enter Your Password"/>
-            <button>Login</button>
+            
+            <button className='bg-yellow-500'>Login</button>
+
         </div>
     )
 }
