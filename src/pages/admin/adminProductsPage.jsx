f3b6935e2f8e8df8b94d49a6424014a3e428f1ff
AmilaWijayananda <amisampath12@gmail.com>
2025-02-02 08:15:53 +0530

    create product table in admin panel 👌

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -6,68 +6,7 @@ import { FaPencil } from "react-icons/fa6";
 export default function AdminProductsPage() {
 
     //getProducts();
-    const [products, setProducts] = useState([
-        {
-            "_id": "6785e6c5cd79ba843d49d07b",
-            "productID": "BP001234",
-            "ProductName": "Luxury Hydrating Face Cream",
-            "altName": [
-                "Moisturizing Cream",
-                "Face Hydration Gel",
-                "Daily Glow Cream"
-            ],
-            "images": [
-                "https://example.com/images/face-cream-front.jpg",
-                "https://example.com/images/face-cream-back.jpg",
-                "https://example.com/images/face-cream-usage.jpg"
-            ],
-            "price": 29.99,
-            "lastPrice": 34.99,
-            "stock": 150,
-            "description": "A deeply hydrating face cream infused with natural extracts to leave your skin glowing and soft. Perfect for daily use on all skin types.",
-            "__v": 0
-        },
-        {
-            "_id": "678b442370396d24e842ed36",
-            "productID": "BP001235",
-            "ProductName": "Luxury Hydrating Foot Cream",
-            "altName": [
-                "Moisturizing Cream",
-                "Face Hydration Gel",
-                "Daily Glow Cream"
-            ],
-            "images": [
-                "https://example.com/images/face-cream-front.jpg",
-                "https://example.com/images/face-cream-back.jpg",
-                "https://example.com/images/face-cream-usage.jpg"
-            ],
-            "price": 29.99,
-            "lastPrice": 34.99,
-            "stock": 150,
-            "description": "A deeply hydrating face cream infused with natural extracts to leave your skin glowing and soft. Perfect for daily use on all skin types.",
-            "__v": 0
-        },
-        {
-            "_id": "678b445670396d24e842ed38",
-            "productID": "BP001236",
-            "ProductName": "Luxury Hydrating Lips Cream",
-            "altName": [
-                "Moisturizing Cream",
-                "Face Hydration Gel",
-                "Daily Glow Cream"
-            ],
-            "images": [
-                "https://example.com/images/face-cream-front.jpg",
-                "https://example.com/images/face-cream-back.jpg",
-                "https://example.com/images/face-cream-usage.jpg"
-            ],
-            "price": 29.99,
-            "lastPrice": 34.99,
-            "stock": 150,
-            "description": "A deeply hydrating face cream infused with natural extracts to leave your skin glowing and soft. Perfect for daily use on all skin types.",
-            "__v": 0
-        }
-    ]);
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:5000/api/products').then
