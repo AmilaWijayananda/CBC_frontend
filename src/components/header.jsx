90b12418cd527606cdf47fbcad9a2bcc0faee523
AmilaWijayananda <amisampath12@gmail.com>
2025-02-04 11:26:20 +0530

    modify header component ✅

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
-        <header className="bg-secondary w-full h-[50px]">
+        <header className="bg-secondary w-full h-[100px] relative flex justify-center items-center">
+            <img src="/logo.png" className="cursor-pointer h-full rounded-full absolute left-[10px]"/>
+
+            <div className="h-full flex items-center w-[500px] justify-evenly">
+            <Link to = "/" className="text-accent text-2xl font-bold hover:border-b border-b-primary">Home</Link>
+            <Link to = "/products" className="text-accent text-2xl font-bold hover:border-b border-b-primary">Products</Link>
+            <Link to = "/about" className="text-accent text-2xl font-bold hover:border-b border-b-primary">About US</Link>
+            <Link to = "/contact" className="text-accent text-2xl font-bold hover:border-b border-b-primary">Contact US</Link>
+            </div>
 
         </header>
     )
