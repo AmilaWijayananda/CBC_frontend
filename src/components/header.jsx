d6a8dbcb5f60a098d8ee0e88f6eb97df7fb9c901
AmilaWijayananda <amisampath12@gmail.com>
2025-02-08 15:47:59 +0530

    Create cart section in header ✅

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,8 +8,9 @@ export default function Header() {
             <div className="h-full flex items-center w-[500px] justify-evenly">
             <Link to = "/" className="text-accent text-2xl font-bold hover:border-b border-b-primary">Home</Link>
             <Link to = "/products" className="text-accent text-2xl font-bold hover:border-b border-b-primary">Products</Link>
-            <Link to = "/about" className="text-accent text-2xl font-bold hover:border-b border-b-primary">About US</Link>
-            <Link to = "/contact" className="text-accent text-2xl font-bold hover:border-b border-b-primary">Contact US</Link>
+            <Link to = "/about" className="text-accent text-2xl font-bold hover:border-b border-b-primary">About</Link>
+            <Link to = "/contact" className="text-accent text-2xl font-bold hover:border-b border-b-primary">Contact Us</Link>
+            <Link to = "/cart" className="text-accent text-2xl font-bold hover:border-b border-b-primary">Cart</Link>
             </div>
 
         </header>
