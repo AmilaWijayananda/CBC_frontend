60390f85cbdb534293f6c2f38cb7d09d1585b706
AmilaWijayananda <amisampath12@gmail.com>
2025-02-08 09:31:45 +0530

    create cart function 👌

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,6 +3,7 @@ import Header from '../components/header';
 import LoginPage from './loginPage';
 import ProductOverview from './productOverview';
 import ProductPage from './home/product';
+import Cart from './home/cart';
 
 export default function HomePage() {
     return (
@@ -12,6 +13,7 @@ export default function HomePage() {
           <Routes path="/*">
             <Route path="/" element={<h1>Home Page</h1>} />
             <Route path='/products' element={<ProductPage/>} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/productInfo/:id" element={<ProductOverview/>} />
           </Routes>
