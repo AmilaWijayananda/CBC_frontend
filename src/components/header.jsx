c028ecd7ee3f35afaebdc166b5c6720506835dcb
AmilaWijayananda <amisampath12@gmail.com>
2025-02-09 09:55:50 +0530

    mobile responsive navBar📲

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { Link } from "react-router-dom";
+import NavSlider from "./navSlider";
 
 export default function Header() {
+    const [isSliderOpen, setIsSliderOpen] = useState(false);
     return (
+        <>
+        {isSliderOpen&&<NavSlider closeSlider={()=>{setIsSliderOpen(false)}}/>}
         <header className="bg-secondary w-full h-[100px] relative flex justify-center items-center">
             <img src="/logo.png" className="cursor-pointer h-full rounded-full absolute left-[10px]"/>
 
-            <div className="h-full flex items-center w-[500px] justify-evenly">
+            <RxHamburgerMenu 
+        onClick={()=>{setIsSliderOpen(true)}}
+        className="text-3xl absolute cursor-pointer text-accent right-[10px] lg:hidden" />
+
+            <div className="h-full flex items-center w-[500px] justify-evenly hidden lg:flex">
             <Link to = "/" className="text-accent text-2xl font-bold hover:border-b border-b-primary">Home</Link>
             <Link to = "/products" className="text-accent text-2xl font-bold hover:border-b border-b-primary">Products</Link>
             <Link to = "/about" className="text-accent text-2xl font-bold hover:border-b border-b-primary">About</Link>
@@ -14,5 +24,6 @@ export default function Header() {
             </div>
 
         </header>
+        </>
     )
 }
