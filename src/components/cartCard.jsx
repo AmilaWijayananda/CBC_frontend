d4555e4e04200d0179aadea67e9fac01fbdcb788
AmilaWijayananda <amisampath12@gmail.com>
2025-02-28 16:21:32 +0530

    Apply mobile responsive to CartCard  ✅

--- src/components/cartCard.jsx
+++ src/components/cartCard.jsx
@@ -8,6 +8,7 @@ export default function CartCard(props) {
 
   const [product, setProduct] = useState(null);
   const [loaded, setLoaded] = useState(false);
+
   useEffect(() => {
     if (!loaded) {
       axios
@@ -15,7 +16,6 @@ export default function CartCard(props) {
         .then((response) => {
           if (response.data != null) {
             setProduct(response.data);
-            console.log(response.data , "product");
             setLoaded(true);
           } else {
             deleteItem(productId);
@@ -25,26 +25,35 @@ export default function CartCard(props) {
           console.log(error);
         });
     }
-  }, []);
+  }, [loaded, productId]);
 
   return (
     <>
       {!loaded ? (
-        <tr>loading</tr>
+        <tr>
+          <td colSpan="6" className="text-center">
+            <div className="flex justify-center items-center p-4">
+              <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-PrimaryGold"></div>
+            </div>
+          </td>
+        </tr>
       ) : (
-        <tr className="hover:bg-accent hover:text-white cursor-pointer">
-          <td className="">
+        <tr className="hover:bg-SecondaryBackground transition-all duration-300">
+          <td className="p-2">
             <img
               src={product?.images[0]}
-              className="w-[90px] h-[90px] object-cover mx-auto"
+              alt={product?.productName}
+              className="w-[90px] h-[90px] object-cover mx-auto rounded-lg"
             />
           </td>
-          <td className="text-center">{product?.productName}</td>
-          <td className="text-center">{productId}</td>
-          <td className="text-center">{qty}</td>
-          <td className="text-center">LKR. {product?.lastPrice.toFixed(2)}</td>
-          <td className="text-center">
-            {(product?.lastPrice * qty).toFixed(2)}
+          <td className="p-2 text-center text-Text">{product?.productName}</td>
+          <td className="p-2 text-center text-Text">{productId}</td>
+          <td className="p-2 text-center text-Text">{qty}</td>
+          <td className="p-2 text-center text-Text">
+            LKR. {product?.lastPrice.toFixed(2)}
+          </td>
+          <td className="p-2 text-center text-Text">
+            LKR. {(product?.lastPrice * qty).toFixed(2)}
           </td>
         </tr>
       )}
