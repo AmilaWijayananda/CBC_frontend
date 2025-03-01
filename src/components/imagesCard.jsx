a1ed22aa3ffc539f08ef0fcdae1a7567370d6704
AmilaWijayananda <amisampath12@gmail.com>
2025-03-01 07:53:20 +0530

    Appy loading effect for image card ✅

--- src/components/imagesCard.jsx
+++ src/components/imagesCard.jsx
@@ -1,13 +1,33 @@
-export default function ImageCard({ image }) {
-    if (!image) return null;
+import { useState } from "react";
 
-    return (
-        <div className="w-full h-full">
-            <img
-                src={image}
-                className="w-full h-full object-cover"
-                alt="Product"
-            />
+export default function ImageCard({ image }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  if (!image) return null;
+
+  // Handle image load
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="w-full h-full bg-Background flex items-center justify-center">
+      {/* Loading Spinner */}
+      {isLoading && (
+        <div className="flex items-center justify-center">
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-PrimaryGold"></div>
         </div>
-    );
+      )}
+
+      {/* Image */}
+      <img
+        src={image}
+        alt="Product"
+        className={`w-full h-full object-cover ${
+          isLoading ? "hidden" : "block"
+        }`}
+        onLoad={handleImageLoad}
+      />
+    </div>
+  );
 }
